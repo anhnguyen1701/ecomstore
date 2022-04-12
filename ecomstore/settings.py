e9f341928d947dd4a632f39342abc7a0556da9b0
anh nguyen <nguyenlamanh1701@gmail.com>
2022-04-13 00:30:46 +0700

    settings static

--- ecomstore/settings.py
+++ ecomstore/settings.py
@@ -132,4 +132,12 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static'
+STATICFILES_DIRS = [
+    'ecomstore/static'
+]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
